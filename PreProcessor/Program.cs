b1966f3097e6b2a05bfc3bb8386d7481e6859036
Vincent Biret <vibiret@microsoft.com>
2024-11-22 14:38:49 -0500

    feat: initial implementation of the console infrastructure
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

---
+++ PreProcessor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.CommandLine;
+using PreProcessor;
+
+var descriptionSourceArgument = new Argument<string>("description-source") {
+	Description = "The path or URL to the file containing the description of the API.",
+	Arity = ArgumentArity.ExactlyOne,
+};
+var outputPathOption = new Option<string>("--output-path", () => "./output.yaml", "The path to the output file.") {
+	Description = "The path to the output file.",
+	Arity = ArgumentArity.ExactlyOne,
+};
+outputPathOption.AddAlias("-o");
+var rootCommand = new RootCommand() {
+	descriptionSourceArgument,
+	outputPathOption,
+};
+rootCommand.Handler = new RootCommandHandler() {
+	DescriptionSource = descriptionSourceArgument,
+	OutputPath = outputPathOption,
+};
+return await rootCommand.InvokeAsync(args);
---
+++ PreProcessor/RootCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.CommandLine;
+using System.CommandLine.Invocation;
+
+namespace PreProcessor;
+
+public class RootCommandHandler : ICommandHandler
+{
+	public required Argument<string> DescriptionSource { get; init; }
+	public required Option<string> OutputPath { get; init; }
+    public int Invoke(InvocationContext context)
+    {
+        throw new InvalidOperationException("This handler should not be invoked. Use the async one instead.");
+    }
+
+    public async Task<int> InvokeAsync(InvocationContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
